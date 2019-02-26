3f1512903d48e7a2d0b33b37271f54a7e0bcd1be
Elias Capasso <elias_capasso@live.com>
2019-02-26 12:00:02 -0300

    initial commit

--- app/src/main/java/com/eliascapasso/auxilio/LoginActivity.java
+++ app/src/main/java/com/eliascapasso/auxilio/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.eliascapasso.auxilio;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,6 +16,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edtPass;
     private Button btnIniciarSesion;
     private Button btnRegistrarse;
+    private Switch swRecordarInicio;
+
+    private static String PREFS_KEY = "login_preferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +29,45 @@ public class LoginActivity extends AppCompatActivity {
         edtPass = (EditText) findViewById(R.id.edtPass);
         btnIniciarSesion = (Button) findViewById(R.id.btnRegistrar);
         btnRegistrarse = (Button) findViewById(R.id.btnRegistrarse);
+        swRecordarInicio = (Switch) findViewById(R.id.swSesionIniciada);
+
+        inicializar();
 
         iniciarSesion();
 
         registrarse();
     }
 
+    private void inicializar(){
+        edtEmail.setText(obtenerLoginSharedPreferencesString(getApplicationContext(),"email"));
+        edtPass.setText(obtenerLoginSharedPreferencesString(getApplicationContext(),"pass"));
+        swRecordarInicio.setChecked(obtenerLoginSharedPreferencesCheckBoxRecordar(getApplicationContext(), "recordar"));
+
+        if(swRecordarInicio.isChecked()){
+            Intent main = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(main);
+            finish();
+        }
+    }
+
     private void iniciarSesion(){
         btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String email = edtEmail.getText().toString();
+                String pass = edtPass.getText().toString();
+
+                //Preferencias compartidas
+                if(swRecordarInicio.isChecked()){
+                    guardarLoginSharedPreferences(email, pass);
+                }
+                else{
+                    guardarLoginSharedPreferences("", "");
+                }
+
                 Intent main = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(main);
+                finish();
             }
         });
     }
@@ -47,5 +80,32 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(reg);
             }
         });
+    }
+
+    private void guardarLoginSharedPreferences(String email, String pass) {
+        SharedPreferences sharedPref = getSharedPreferences("login_preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("email", email);
+        editor.putString("pass", pass);
+
+        if(email.equals("") && pass.equals("")){
+            editor.putBoolean("recordar", false);
+        }
+        else{
+            editor.putBoolean("recordar", true);
+        }
+
+        editor.apply();
+        editor.commit();
+    }
+
+    public static String obtenerLoginSharedPreferencesString(Context context, String keyPref) {
+        SharedPreferences preferences = context.getSharedPreferences(PREFS_KEY, MODE_PRIVATE);
+        return  preferences.getString(keyPref, "");
+    }
+
+    public static boolean obtenerLoginSharedPreferencesCheckBoxRecordar(Context context, String keyPref) {
+        SharedPreferences preferences = context.getSharedPreferences(PREFS_KEY, MODE_PRIVATE);
+        return  preferences.getBoolean(keyPref, false);
     }
 }
--- app/src/main/java/com/eliascapasso/auxilio/MainActivity.java
+++ app/src/main/java/com/eliascapasso/auxilio/MainActivity.java
@@ -1,8 +1,10 @@
 package com.eliascapasso.auxilio;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -15,6 +17,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.eliascapasso.auxilio.R;
 
@@ -38,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         setupNavigationDrawerContent(navView);
 
-        setFragment(0);
+        setFragment(1);
     }
 
     private void setupNavigationDrawerContent(NavigationView navigationView) {
@@ -47,18 +50,30 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
                         switch (menuItem.getItemId()) {
-                            case R.id.optAlumno:
+                            case R.id.optMiPerfil:
                                 getSupportFragmentManager().beginTransaction().
                                         remove(getSupportFragmentManager().findFragmentById(R.id.contenido)).commit();
                                 menuItem.setChecked(true);
                                 setFragment(0);
                                 drawerLayout.closeDrawer(GravityCompat.START);
                                 return true;
-                            case R.id.optProfesor:
+                            case R.id.optAlumno:
                                 getSupportFragmentManager().beginTransaction().
                                         remove(getSupportFragmentManager().findFragmentById(R.id.contenido)).commit();
                                 menuItem.setChecked(true);
                                 setFragment(1);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.optProfesor:
+                                getSupportFragmentManager().beginTransaction().
+                                        remove(getSupportFragmentManager().findFragmentById(R.id.contenido)).commit();
+                                menuItem.setChecked(true);
+                                setFragment(2);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.optCerrarSesion:
+                                menuItem.setChecked(true);
+                                setFragment(3);
                                 drawerLayout.closeDrawer(GravityCompat.START);
                                 return true;
                         }
@@ -74,12 +89,20 @@ public class MainActivity extends AppCompatActivity {
             case 0:
                 fragmentManager = getSupportFragmentManager();
                 fragmentTransaction = fragmentManager.beginTransaction();
+                EditarPerfilFragment editarPerfilFragment = new EditarPerfilFragment();
+                fragmentTransaction.replace(R.id.contenido, editarPerfilFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                break;
+            case 1:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
                 AlumnoFragment alumnoFragment = new AlumnoFragment();
                 fragmentTransaction.replace(R.id.contenido, alumnoFragment);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
                 break;
-            case 1:
+            case 2:
                 fragmentManager = getSupportFragmentManager();
                 fragmentTransaction = fragmentManager.beginTransaction();
                 ProfesorFragment profesorFragment = new ProfesorFragment();
@@ -87,6 +110,44 @@ public class MainActivity extends AppCompatActivity {
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
                 break;
+            case 3:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Cerrar Sesión");
+                builder.setMessage("¿Seguro que desea cerrar sesión?");
+                builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Destilda el recordar inicio sesion
+                        SharedPreferences sharedPref = getSharedPreferences("login_preferences", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putBoolean("recordar", false);
+                        editor.apply();
+                        editor.commit();
+
+                        finishAffinity();
+                        Intent loginActivity = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(loginActivity);
+                    }
+                });
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //No hace nada
+                    }
+                });
+                builder.show();
+                break;
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (tiempoPrimerClick + INTERVALO > System.currentTimeMillis()){
+            finishAffinity();
+            return;
+        }else {
+            Toast.makeText(this, "Vuelve a presionar para salir", Toast.LENGTH_SHORT).show();
+        }
+        tiempoPrimerClick = System.currentTimeMillis();
     }
 }
